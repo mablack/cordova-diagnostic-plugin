b0008e0011f683ad1719b0db13c5da0676d84868
Dave Alden <dpa99c@gmail.com>
2015-11-17 11:49:40 +0000

    Merge Windows 10 Mobile pull request with initial work on Android 6 runtime permissions

--- www/android/diagnostic.js
+++ www/android/diagnostic.js
@@ -7,6 +7,45 @@
 var Diagnostic = function(){};
 
 /**
+ * Android 6.0 (API 23) "dangerous" permissions that need to be requested at run-time
+ * See http://developer.android.com/guide/topics/security/permissions.html#perm-groups
+ * @type {Object}
+ */
+Diagnostic.runtimePermission = {
+	"READ_CALENDAR": 0,
+	"WRITE_CALENDAR": 1,
+	"CAMERA": 2,
+	"READ_CONTACTS": 3,
+	"WRITE_CONTACTS": 4,
+	"GET_ACCOUNTS": 5,
+	"ACCESS_FINE_LOCATION": 6,
+	"ACCESS_COARSE_LOCATION": 7,
+	"RECORD_AUDIO": 8,
+	"READ_PHONE_STATE": 9,
+	"CALL_PHONE": 10,
+	"READ_CALL_LOG": 11,
+	"WRITE_CALL_LOG": 12,
+	"ADD_VOICEMAIL": 13,
+	"USE_SIP": 14,
+	"PROCESS_OUTGOING_CALLS": 15,
+	"BODY_SENSORS": 16,
+	"SEND_SMS": 17,
+	"RECEIVE_SMS": 18,
+	"READ_SMS": 19,
+	"RECEIVE_WAP_PUSH": 20,
+	"RECEIVE_MMS": 21,
+	"READ_EXTERNAL_STORAGE": 22,
+	"WRITE_EXTERNAL_STORAGE": 23
+};
+
+Diagnostic.runtimePermissionResponseCode = {
+	0: "ALREADY_GRANTED",
+	1: "PERMISSION_GRANTED",
+	2: "PERMISSON_DENIED",
+	3: "PERMISSION_DENIED_ALWAYS"
+};
+
+/**
  * Checks if location is enabled.
  * On Android, this returns true if Location Mode is enabled and any mode is selected (e.g. Battery saving, Device only, High accuracy)
  *
@@ -205,6 +244,35 @@ Diagnostic.prototype.setBluetoothState = function(successCallback, errorCallback
 		'setBluetoothState',
 		[state]);
 };
+
+
+/**
+ * Requests app to be granted access to a dangerous permission.
+ * If the permission has already been granted or the device is running Android 5.1 (API level 22) or lower,
+ * @param {Function} successCallback - function to call on successful request for runtime permissions.
+ * @param errorCallback
+ * @param permission
+ */
+Diagnostic.prototype.requestRuntimePermission = function(successCallback, errorCallback, permission) {};
+
+Diagnostic.prototype.requestRuntimePermissions = function(successCallback, errorCallback, permissions) {};
+
+Diagnostic.prototype.getPermissionAuthorizationStatus = function(successCallback, errorCallback, permission) {};
+
+Diagnostic.prototype.getPermissionsAuthorizationStatus = function(successCallback, errorCallback, permissions) {};
+
+/**************************
+ * iOS runtime equivalents
+ **************************/
+
+Diagnostic.prototype.requestLocationAuthorization = function(successCallback, errorCallback) {};
+
+Diagnostic.prototype.getLocationAuthorizationStatus = function(successCallback, errorCallback) {};
+
+Diagnostic.prototype.requestCameraAuthorization = function(successCallback, errorCallback) {};
+
+Diagnostic.prototype.getCameraAuthorizationStatus = function(successCallback, errorCallback) {};
+
 
 module.exports = new Diagnostic();
 
