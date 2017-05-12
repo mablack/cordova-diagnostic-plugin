ded53d29aeea1cb1a74ca2f9dfd81b1c3386465e
Dave Alden <dpa99c@gmail.com>
2017-05-12 14:32:20 +0100

    Fix bug in mapping of legacy camera API function signatures for iOS and Windows. 8c81eaaa889e54bfc875956c29e81793ec4eadef only fixed it for Android.

--- www/windows/diagnostic.js
+++ www/windows/diagnostic.js
@@ -5,14 +5,16 @@
 **/
 var Diagnostic = function () { };
 
-function mapFromLegacyCameraApi(params) {
-    params = params || {};
+function mapFromLegacyCameraApi() {
+    var params;
     if (typeof arguments[0]  === "function") {
         params = (arguments.length > 2 && typeof arguments[2]  === "object") ? arguments[2] : {};
         params.successCallback = arguments[0];
         if(arguments.length > 1 && typeof arguments[1]  === "function") {
             params.errorCallback = arguments[1];
         }
+    }else { // if (typeof arguments[0]  === "object")
+        params = arguments[0];
     }
     return params;
 }
