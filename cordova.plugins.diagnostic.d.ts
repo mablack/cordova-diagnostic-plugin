f4332b97473c64ad71b83bed9142c76c2362514a
Dave Alden <dpa99c@gmail.com>
2018-03-01 22:22:37 +0000

    update types version

--- cordova.plugins.diagnostic.d.ts
+++ cordova.plugins.diagnostic.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for cordova.plugins.diagnostic v3.6.x
+// Type definitions for cordova.plugins.diagnostic v4
 // Project: https://github.com/dpa99c/cordova-diagnostic-plugin
 // Definitions by: Dave Alden <https://github.com/dpa99c/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
